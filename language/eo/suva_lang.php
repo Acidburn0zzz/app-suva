3f94c4f821d3939f8f06390719d4fbe9e69caedb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 05:57:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suvo provizas tunelo kaj ĉifrado servoj al ClearCenter portalo';
+$lang['suva_app_name'] = 'Suvo';
+$lang['suva_device_name_invalid'] = 'Aparatnomo ne estas valida.';
+$lang['suva_hostkey_invalid'] = 'Hostkey estas nevalida.';
