87991b2a42157a3ce79bb7638048c31fff57a195
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:27:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'ຊູວາໃຫ້ tunnel ແລະການເຂົ້າລະຫັດການບໍລິການ ClearCenter portal';
+$lang['suva_app_name'] = 'ຊູວາ';
+$lang['suva_device_name_invalid'] = 'ຊື່ອຸປະກອນບໍ່ຖືກຕ້ອງ.';
+$lang['suva_hostkey_invalid'] = 'Hostkey ແມ່ນບໍ່ຖືກຕ້ອງ.';
