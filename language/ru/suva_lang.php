6406a466782afd844fa3b1339f56b1392d214d60
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:49:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Сува предоставляет туннель и шифрования услуги портала ClearCenter';
+$lang['suva_app_name'] = 'Сува';
+$lang['suva_device_name_invalid'] = 'Имя устройства является недействительным.';
+$lang['suva_hostkey_invalid'] = 'HOSTKEY является недействительным.';
