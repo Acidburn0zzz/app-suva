cb750e5615c1a63f56acbaf16dfbcd3103ce18f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 05:48:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva ClearCenter ပေါ်တယ်မှဥမင်လိုဏ်ခေါင်းများနှင့်စာဝှက်စနစ်န်ဆောင်မှုကိုပေးစွမ်းသည်';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'device name ကိုမမှန်ကန်ဘူး။';
+$lang['suva_hostkey_invalid'] = 'Hostkey မမှန်ကန်ဘူး။';
