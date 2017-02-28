7b20270b62bd852f746d1dfef7c50c07350f541b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:28:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva suteikia tunelių ir šifravimo paslaugas ClearCenter portalo';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Įrenginio pavadinimas yra neteisingas.';
+$lang['suva_hostkey_invalid'] = 'Hostkey negalioja.';
