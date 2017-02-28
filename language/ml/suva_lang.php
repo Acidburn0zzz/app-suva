193e1a5fad9e5c05cab9dccd21e443770c31f5b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:34:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva ൽ ClearCenter കവാടം തുരങ്കം എൻക്രിപ്ഷൻ സേവനങ്ങൾ നൽകുന്നു';
+$lang['suva_app_name'] = 'Suva ൽ';
+$lang['suva_device_name_invalid'] = 'ഉപകരണ പേര് അസാധുവാണ്.';
+$lang['suva_hostkey_invalid'] = 'Hostkey അസാധുവാണ്.';
