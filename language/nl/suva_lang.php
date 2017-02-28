95b71f262d499991fb1d3f607c4e20a295f54c9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:41:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva biedt tunnel en encryptie-diensten aan ClearCenter portal';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Apparaatnaam is ongeldig.';
+$lang['suva_hostkey_invalid'] = 'Hostkey is ongeldig.';
