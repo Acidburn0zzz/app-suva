1b915575b60805ca57c9693c92fe5677f5246405
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:19:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'スバはClearCenterポータルへのトンネルと暗号化サービスを提供しています';
+$lang['suva_app_name'] = 'スバ';
+$lang['suva_device_name_invalid'] = 'デバイス名が無効です。';
+$lang['suva_hostkey_invalid'] = 'ホスト鍵が無効です。';
