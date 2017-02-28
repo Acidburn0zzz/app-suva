37b5f84bad054400e418529752bc934d17314ad1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:35:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Сувагийн ClearCenter порталд туннель болон шифрлэх үйлчилгээ үзүүлдэг';
+$lang['suva_app_name'] = 'Сувагийн';
+$lang['suva_device_name_invalid'] = 'Төхөөрөмжийн нэр хүчин төгөлдөр бус байна.';
+$lang['suva_hostkey_invalid'] = 'Hostkey хүчин төгөлдөр бус байна.';
