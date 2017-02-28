2387c3f6cb0c9a4087d36be923a0fc6764174cbd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:29:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'ซูวาให้อุโมงค์และการเข้ารหัสการบริการให้กับ ClearCenter พอร์ทัล';
+$lang['suva_app_name'] = 'ซูวา';
+$lang['suva_device_name_invalid'] = 'ชื่ออุปกรณ์ไม่ถูกต้อง';
+$lang['suva_hostkey_invalid'] = 'Hostkey ไม่ถูกต้อง';
