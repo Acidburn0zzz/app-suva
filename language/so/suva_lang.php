2ae8fdd5c953c8a9fe0982ac70f469a6bbb38e27
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:54:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva bixisaa adeegyo tunnel iyo sirta inay portal ClearCenter';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Magaca Device waa baadil.';
+$lang['suva_hostkey_invalid'] = 'Hostkey waa baadil.';
