c76a06fbe8527ed080431d15a3699acf8fa9ddec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:25:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = '수바는 ClearCenter 포털에 터널 및 암호화 서비스를 제공합니다';
+$lang['suva_app_name'] = '수바';
+$lang['suva_device_name_invalid'] = '장치 이름이 잘못되었습니다.';
+$lang['suva_hostkey_invalid'] = '호스트 키가 잘못되었습니다.';
