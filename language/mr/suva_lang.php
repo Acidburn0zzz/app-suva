0b183db2563d28f21b89c775dc81089b2ae88617
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:36:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva ClearCenter पोर्टल बोगदा आणि कूटबद्धीकरण सेवा उपलब्ध';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'साधन नाव अवैध आहे.';
+$lang['suva_hostkey_invalid'] = 'Hostkey अवैध आहे.';
