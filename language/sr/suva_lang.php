db31a9e784abfd8b951130998e9ce2487d8fa1be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:50:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Сува даје тунел и енкрипције услуге ЦлеарЦентер портал';
+$lang['suva_app_name'] = 'сува';
+$lang['suva_device_name_invalid'] = 'Име уређаја је неважећи.';
+$lang['suva_hostkey_invalid'] = 'Хосткеи је неважећи.';
