73dd658f88dd74e90bb336300acca2064f52b354
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 15:29:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva ClearCenter portalına tünel ve şifreleme hizmetleri vermektedir';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Cihaz adı geçersiz.';
+$lang['suva_hostkey_invalid'] = 'Konak anahtarı geçersiz.';
