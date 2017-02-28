7833438e88040b0feb8e84152ea0aac2485a88fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:56:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva ofrece servicios de túnel y cifrado en el portal ClearCenter';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Nombre del dispositivo no es válido.';
+$lang['suva_hostkey_invalid'] = 'HostKey no es válido.';
