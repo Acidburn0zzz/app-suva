845d5cf5e0a2c3ec4371a69d8455551889f4ab67
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 06:10:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'सुवा ClearCenter पोर्टल के लिए सुरंग और एन्क्रिप्शन सेवाएं प्रदान करता है';
+$lang['suva_app_name'] = 'सुवा';
+$lang['suva_device_name_invalid'] = 'डिवाइस का नाम अवैध है।';
+$lang['suva_hostkey_invalid'] = 'Hostkey अमान्य है।';
