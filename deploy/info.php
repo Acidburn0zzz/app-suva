4988c22915ea721275e66c86bb6ef3dbc7f7071a
pcbaldwin <peter@egloo.ca>
2017-08-12 22:34:13 -0400

    Follow-up to onboot feature [tracker #12061]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'suva';
-$app['version'] = '2.3.22';
+$app['version'] = '2.4.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-suva.spec
+++ packaging/app-suva.spec
@@ -1,7 +1,7 @@
 
 Name: app-suva
 Epoch: 1
-Version: 2.3.22
+Version: 2.4.0
 Release: 1%{dist}
 Summary: Suva - Core
 License: Proprietary
