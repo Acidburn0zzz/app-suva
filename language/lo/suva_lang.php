20ccdf6b5506495cdfd631124d52a1940a3ef558
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:26:36 +0000

    Add new files and directories, commit changes, push changes

--- language/lo/suva_lang.php
+++ language/lo/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'ຊູວາໃຫ້ tunnel ແລະການເຂົ້າລະຫັດການບໍລິການ ClearCenter portal';
-$lang['suva_app_name'] = 'ຊູວາ';
+$lang['suva_app_description'] = 'Suva ສະຫນອງການບໍລິການການໂອນເງິນແລະລະບົບການເຂົ້າລະຫັດລັບທີ່ Port ClearCenter';
+$lang['suva_app_name'] = 'Suva';
 $lang['suva_device_name_invalid'] = 'ຊື່ອຸປະກອນບໍ່ຖືກຕ້ອງ.';
-$lang['suva_hostkey_invalid'] = 'Hostkey ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['suva_hostkey_invalid'] = 'Hostkey ບໍ່ຖືກຕ້ອງ.';
