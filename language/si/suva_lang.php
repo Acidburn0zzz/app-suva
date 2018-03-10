cc9aedc2fc3faf7e6a7f8e62d74685e2ac5ab8f6
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:39:59 +0000

    Add new files and directories, commit changes, push changes

--- language/si/suva_lang.php
+++ language/si/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'සුව ClearCenter බිහිදොර කිරීමට උමග හා ගුප්ත කේතනය සේවාවන් සපයයි';
-$lang['suva_app_name'] = 'සුව';
-$lang['suva_device_name_invalid'] = 'උපාංගයේ නම වලංගු නැත.';
-$lang['suva_hostkey_invalid'] = 'Hostkey වලංගු නැත.';
+$lang['suva_app_description'] = 'සෙවාව ClearCenter ද්වාරය වෙත උමං මාර්ග සහ එන්ක්රිප්ට් සේවාවන් සපයයි';
+$lang['suva_app_name'] = 'සුවා';
+$lang['suva_device_name_invalid'] = 'උපාංග නාමය වලංගු නැත.';
+$lang['suva_hostkey_invalid'] = 'ගොනුවේ වැරදියි.';
