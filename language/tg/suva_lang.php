5fa0fea3f99cfc02bd28a8f885af484da26eab99
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:46:40 +0000

    Add new files and directories, commit changes, push changes

--- language/tg/suva_lang.php
+++ language/tg/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Сува таъмин нақб ва рамзгузорӣ хизматрасонӣ ба ClearCenter портали';
-$lang['suva_app_name'] = 'Сува';
-$lang['suva_device_name_invalid'] = 'номи дастгоҳ нодуруст аст.';
+$lang['suva_app_description'] = 'Suva ба ClearCenter портали хидматҳои нақб ва рамзгузорӣ пешниҳод мекунад';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Номи дастгоҳ беэътибор аст.';
 $lang['suva_hostkey_invalid'] = 'Hostkey нодуруст аст.';
