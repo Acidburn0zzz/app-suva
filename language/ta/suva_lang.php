82b4a852be1477fad57ffd3911805daeb4c1f7d4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:45:28 +0000

    Add new files and directories, commit changes, push changes

--- language/ta/suva_lang.php
+++ language/ta/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'பிஜி ClearCenter போர்டல் சுரங்கப்பாதை மற்றும் குறியாக்க சேவைகளை வழங்குகிறது';
+$lang['suva_app_description'] = 'சுவா, சுத்திகரிப்பு மற்றும் குறியாக்க சேவைகளை ClearCenter Portal க்கு வழங்குகிறது';
 $lang['suva_app_name'] = 'பிஜி';
 $lang['suva_device_name_invalid'] = 'சாதனத்தின் பெயர் தவறானது.';
 $lang['suva_hostkey_invalid'] = 'Hostkey தவறானது.';
