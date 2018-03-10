581bc810ec2b1c0c8adc521d9cd22669a9d71280
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:04:57 +0000

    Add new files and directories, commit changes, push changes

--- language/be/suva_lang.php
+++ language/be/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Сува прадастаўляе тунэль і шыфраванні паслугі партала ClearCenter';
+$lang['suva_app_description'] = 'Сува прадастаўляе тунэль і шыфраванні паслугі ClearCenter партала';
 $lang['suva_app_name'] = 'Сува';
 $lang['suva_device_name_invalid'] = 'Імя прылады з&#39;яўляецца несапраўдным.';
 $lang['suva_hostkey_invalid'] = 'HOSTKEY з&#39;яўляецца несапраўдным.';
