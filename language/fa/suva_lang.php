6497ac15a7af9f43556f03911966ca6d7d2c520f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:36:07 +0000

    Add new files and directories, commit changes, push changes

--- language/fa/suva_lang.php
+++ language/fa/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'سووا فراهم می کند تونل و رمزگذاری خدمات به ClearCenter پورتال';
+$lang['suva_app_description'] = 'سووا خدمات تونل و رمزگذاری را به پورتال ClearCenter ارائه می دهد';
 $lang['suva_app_name'] = 'سووا';
-$lang['suva_device_name_invalid'] = 'نام دستگاه نامعتبر است.';
-$lang['suva_hostkey_invalid'] = 'Hostkey نامعتبر است.';
+$lang['suva_device_name_invalid'] = 'نام دستگاه نامعتبر است';
+$lang['suva_hostkey_invalid'] = 'میزبان کلید نامعتبر است';
