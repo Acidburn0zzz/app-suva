56dcf851be78b3d578d8528812764281ea0c046b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:21:07 +0000

    Add new files and directories, commit changes, push changes

--- language/is/suva_lang.php
+++ language/is/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva veitir göng og dulkóðun þjónustu ClearCenter vefsíðunni';
+$lang['suva_app_description'] = 'Suva veitir göng- og dulkóðunarþjónustu til ClearCenter vefgáttarinnar';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'tækjaskrá var ekki gild.';
+$lang['suva_device_name_invalid'] = 'Tæki heiti er ógilt.';
 $lang['suva_hostkey_invalid'] = 'Hostkey er ógilt.';
