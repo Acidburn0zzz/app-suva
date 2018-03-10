add695fa2571e5b311b76c9cbcc32543720c4e6c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:51:04 +0000

    Add new files and directories, commit changes, push changes

--- language/vi/suva_lang.php
+++ language/vi/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva cung cấp dịch vụ đường hầm và mã hóa để cổng thông tin ClearCenter';
+$lang['suva_app_description'] = 'Suva cung cấp dịch vụ đường hầm và mã hóa cho cổng ClearCenter';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'Tên thiết bị là không hợp lệ.';
-$lang['suva_hostkey_invalid'] = 'Hostkey là không hợp lệ.';
+$lang['suva_device_name_invalid'] = 'Tên thiết bị không hợp lệ.';
+$lang['suva_hostkey_invalid'] = 'Hostkey không hợp lệ.';
