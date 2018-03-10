73fd2e605b947dd0ade8a121b7d6d1e845095725
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:01:23 +0000

    Add new files and directories, commit changes, push changes

--- language/af/suva_lang.php
+++ language/af/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva bied tonnel en kodering dienste aan ClearCenter portaal';
+$lang['suva_app_description'] = 'Suva bied tonnel- en enkripsiedienste aan die ClearCenter-portaal';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'toestel naam is nie geldig.';
+$lang['suva_device_name_invalid'] = 'Toestelnaam is ongeldig.';
 $lang['suva_hostkey_invalid'] = 'Hostkey is ongeldig.';
