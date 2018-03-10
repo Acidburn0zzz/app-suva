eabb93e1770d67db14ae0ce3d58fc3c9dad6335c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:23:44 +0000

    Add new files and directories, commit changes, push changes

--- language/kk/suva_lang.php
+++ language/kk/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva ClearCenter порталына тоннель және шифрлау қызметтерін ұсынады';
+$lang['suva_app_description'] = 'Suva ClearCenter порталына туннельдер мен шифрлау қызметтерін ұсынады';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'Құрылғы атауы жарамсыз болып табылады.';
-$lang['suva_hostkey_invalid'] = 'Hostkey жарамсыз болып табылады.';
+$lang['suva_device_name_invalid'] = 'Құрылғының атауы жарамсыз.';
+$lang['suva_hostkey_invalid'] = 'Негізгі түйме жарамсыз.';
