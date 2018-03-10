50ad85d93aeef5f1b99271087823cbfedc6b6c4e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:22:58 +0000

    Add new files and directories, commit changes, push changes

--- language/ka/suva_lang.php
+++ language/ka/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva უზრუნველყოფს გვირაბი და კოდირების მომსახურება ClearCenter ვერსია';
-$lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'მოწყობილობა სახელი არასწორია.';
+$lang['suva_app_description'] = 'Suva უზრუნველყოფს გვირაბი და დაშიფვრის მომსახურება ClearCenter ვერსია';
+$lang['suva_app_name'] = 'სუვა';
+$lang['suva_device_name_invalid'] = 'მოწყობილობის სახელი არასწორია.';
 $lang['suva_hostkey_invalid'] = 'Hostkey არასწორია.';
