e613b7265daa9f0218b817347d1a4d8ff0fe70a2
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:24:56 +0000

    Add new files and directories, commit changes, push changes

--- language/kn/suva_lang.php
+++ language/kn/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva ನಿಂದ ClearCenter ಪೋರ್ಟಲ್ ಸುರಂಗ ಮತ್ತು ಗೂಢಲಿಪೀಕರಣ ಸೇವೆಗಳು ಒದಗಿಸುತ್ತದೆ';
-$lang['suva_app_name'] = 'Suva ನಿಂದ';
-$lang['suva_device_name_invalid'] = 'ಸಾಧನದ ಹೆಸರು ಅಮಾನ್ಯ.';
-$lang['suva_hostkey_invalid'] = 'Hostkey ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['suva_app_description'] = 'ಸುವ ಕ್ವೆಸ್ಟ್ ಸೆಂಟರ್ ಪೋರ್ಟಲ್ಗೆ ಸುರಂಗ ಮತ್ತು ಗೂಢಲಿಪೀಕರಣ ಸೇವೆಗಳನ್ನು ಒದಗಿಸುತ್ತದೆ';
+$lang['suva_app_name'] = 'ಸುವ';
+$lang['suva_device_name_invalid'] = 'ಸಾಧನದ ಹೆಸರು ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['suva_hostkey_invalid'] = 'ಹೋಸ್ಟ್ಕೀ ಅಮಾನ್ಯವಾಗಿದೆ.';
