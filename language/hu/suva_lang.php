247104132406b9705969ff603c6dce2f137534b1
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:18:40 +0000

    Add new files and directories, commit changes, push changes

--- language/hu/suva_lang.php
+++ language/hu/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva rendelkezik alagút és titkosítási szolgáltatások ClearCenter portál';
+$lang['suva_app_description'] = 'A Suva alagút- és titkosítási szolgáltatásokat nyújt a ClearCenter portál számára';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'Eszköz neve érvénytelen.';
-$lang['suva_hostkey_invalid'] = 'HostKey érvénytelen.';
+$lang['suva_device_name_invalid'] = 'Az eszköz neve érvénytelen.';
+$lang['suva_hostkey_invalid'] = 'A fogadó kulcs érvénytelen.';
