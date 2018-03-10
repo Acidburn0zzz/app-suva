07a8c33935816b51f5defb18a1c101f6054c5ee9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:07:20 +0000

    Add new files and directories, commit changes, push changes

--- language/my/suva_lang.php
+++ language/my/suva_lang.php
@@ -2,5 +2,5 @@
 
 $lang['suva_app_description'] = 'Suva ClearCenter ပေါ်တယ်မှဥမင်လိုဏ်ခေါင်းများနှင့်စာဝှက်စနစ်န်ဆောင်မှုကိုပေးစွမ်းသည်';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'device name ကိုမမှန်ကန်ဘူး။';
-$lang['suva_hostkey_invalid'] = 'Hostkey မမှန်ကန်ဘူး။';
+$lang['suva_device_name_invalid'] = 'ကိရိယာအမည်ကိုမှားနေပါသည်။';
+$lang['suva_hostkey_invalid'] = 'Hostkey မှားနေပါသည်။';
