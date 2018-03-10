3b6cc05b0136713d2323b058781a3392bddee742
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:52:17 +0000

    Add new files and directories, commit changes, push changes

--- language/yi/suva_lang.php
+++ language/yi/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva גיט טונעל און ענקריפּשאַן באַדינונגען צו קלעאַרסענטער טויער';
+$lang['suva_app_description'] = 'סווואַ גיט טונעל און ענקריפּשאַן באַדינונגען צו קיילאַסטער טויער';
 $lang['suva_app_name'] = 'Suva';
 $lang['suva_device_name_invalid'] = 'מיטל נאָמען איז פאַרקריפּלט.';
 $lang['suva_hostkey_invalid'] = 'האָסטקיי איז פאַרקריפּלט.';
