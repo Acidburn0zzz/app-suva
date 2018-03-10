ac488ad83f1c6527dfaf1914500cd2d41b08541c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:29:05 +0000

    Add new files and directories, commit changes, push changes

--- language/mi/suva_lang.php
+++ language/mi/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva whakarato ratonga kauhanga me te whakamunatanga ki ClearCenter portal';
+$lang['suva_app_description'] = 'Ka whakaratohia e Suva nga ratonga pouaka me te whakamunatanga ki te whaarangi ClearCenter';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'He muhu te ingoa Pūrere.';
-$lang['suva_hostkey_invalid'] = 'He muhu Hostkey.';
+$lang['suva_device_name_invalid'] = 'He muhu te ingoa o te pūrere.';
+$lang['suva_hostkey_invalid'] = 'He muhu te Hostkey.';
