a04a72c2a25e0b06c773e5c59591856f7b3f1165
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:19:55 +0000

    Add new files and directories, commit changes, push changes

--- language/ga/suva_lang.php
+++ language/ga/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Cuireann Suva Seirbhísí tollán agus criptithe a ClearCenter tairseach';
+$lang['suva_app_description'] = 'Soláthraíonn Suva seirbhísí tolláin agus criptithe chuig tairseach ClearCenter';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'ainm Gléas neamhbhailí.';
-$lang['suva_hostkey_invalid'] = 'Is Hostkey neamhbhailí.';
+$lang['suva_device_name_invalid'] = 'Tá ainm an ghléas neamhbhailí.';
+$lang['suva_hostkey_invalid'] = 'Tá an t-óstach neamhbhailí.';
