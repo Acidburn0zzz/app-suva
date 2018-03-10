3ef4b66d5d51e6b82cb4223690f443e16e872549
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:35:22 +0000

    Add new files and directories, commit changes, push changes

--- language/pa/suva_lang.php
+++ language/pa/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva ClearCenter ਪੋਰਟਲ ਦਾ ਸੁਰੰਗ ਅਤੇ ਇੰਕ੍ਰਿਪਸ਼ਨ ਸੇਵਾ ਦਿੰਦਾ ਹੈ';
-$lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'ਜੰਤਰ ਦਾ ਨਾਮ ਗਲਤ ਹੈ.';
-$lang['suva_hostkey_invalid'] = 'Hostkey ਗਲਤ ਹੈ.';
+$lang['suva_app_description'] = 'ਸੁਵਾ ਕਲੀਅਰਕੇਂਟਰ ਪੋਰਟਲ ਲਈ ਸੁਰੰਗ ਅਤੇ ਏਨਕ੍ਰਿਪਸ਼ਨ ਸੇਵਾਵਾਂ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ';
+$lang['suva_app_name'] = 'ਸੁਵਾ';
+$lang['suva_device_name_invalid'] = 'ਡਿਵਾਈਸ ਦਾ ਨਾਮ ਅਵੈਧ ਹੈ.';
+$lang['suva_hostkey_invalid'] = 'ਮੇਜ਼ਬਾਨ ਕੀ ਅਯੋਗ ਹੈ';
