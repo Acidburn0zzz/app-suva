49788c56ff22aa8042dbf01f06ee63b0e283d1b9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:41:51 +0000

    Add new files and directories, commit changes, push changes

--- language/so/suva_lang.php
+++ language/so/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva bixisaa adeegyo tunnel iyo sirta inay portal ClearCenter';
+$lang['suva_app_description'] = 'Suva waxay bixisaa adeegyo tilmaansi ah iyo xarig si loo galo shabakada ClearCenter';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'Magaca Device waa baadil.';
-$lang['suva_hostkey_invalid'] = 'Hostkey waa baadil.';
+$lang['suva_device_name_invalid'] = 'Magaca aaladda waa mid aan shaqeynin.';
+$lang['suva_hostkey_invalid'] = 'Hostkey waa mid aan sharci ahayn.';
