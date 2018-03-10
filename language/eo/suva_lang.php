77ce90f97e273ea16bb0a3130f07a40ec751519f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:11:41 +0000

    Add new files and directories, commit changes, push changes

--- language/eo/suva_lang.php
+++ language/eo/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suvo provizas tunelo kaj ĉifrado servoj al ClearCenter portalo';
-$lang['suva_app_name'] = 'Suvo';
-$lang['suva_device_name_invalid'] = 'Aparatnomo ne estas valida.';
+$lang['suva_app_description'] = 'Suva provizas tunelojn kaj ĉifradajn servojn al ClearCenter-portalo';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Aplika nomo estas nevalida.';
 $lang['suva_hostkey_invalid'] = 'Hostkey estas nevalida.';
