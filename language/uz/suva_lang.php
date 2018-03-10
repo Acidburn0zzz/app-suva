729b6803f55de46ce51ff4d5321134e3eaa1dd04
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:50:18 +0000

    Add new files and directories, commit changes, push changes

---
+++ language/uz/suva_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['suva_app_description'] = 'Suva ClearCenter portaliga tunnel va shifrlash xizmatlarini taqdim etadi';
+$lang['suva_app_name'] = 'Suva';
+$lang['suva_device_name_invalid'] = 'Qurilma nomi noto&#39;g&#39;ri.';
+$lang['suva_hostkey_invalid'] = 'Xost-kod noto&#39;g&#39;ri.';
