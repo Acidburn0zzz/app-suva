f5bf1a317f8a65862a4cdf17a2454eb2d9f0f695
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:46:13 +0000

    Add new files and directories, commit changes, push changes

--- language/te/suva_lang.php
+++ language/te/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'సువా ClearCenter పోర్టల్ సొరంగం మరియు ఎన్క్రిప్షన్ సేవలు అందిస్తుంది';
-$lang['suva_app_name'] = 'సువా';
+$lang['suva_app_description'] = 'సువా క్లియర్సెంట్ పోర్టల్కు సొరంగం మరియు ఎన్క్రిప్షన్ సేవలను అందిస్తుంది';
+$lang['suva_app_name'] = 'Suva';
 $lang['suva_device_name_invalid'] = 'పరికరం పేరు చెల్లనిది.';
 $lang['suva_hostkey_invalid'] = 'Hostkey చెల్లదు.';
