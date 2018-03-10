cec917ab0d87162f26bc47bc836e730e88dfdf9f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:16:35 +0000

    Add new files and directories, commit changes, push changes

--- language/ht/suva_lang.php
+++ language/ht/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva bay tinèl ak chifreman sèvis nan ClearCenter Portal';
+$lang['suva_app_description'] = 'Suva bay tinèl ak sèvis chifreman pou ClearCenter Portal';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'Non Aparèy se valab.';
-$lang['suva_hostkey_invalid'] = 'Hostkey se valab.';
+$lang['suva_device_name_invalid'] = 'Non aparèy la envalid.';
+$lang['suva_hostkey_invalid'] = 'Hostkey se envalid.';
