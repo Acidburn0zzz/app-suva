9ea32d0ebc15f9edfd6d075dd72505459a479035
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:04:13 +0000

    Add new files and directories, commit changes, push changes

--- language/eu/suva_lang.php
+++ language/eu/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva tunel eta enkriptatze zerbitzuak eskaintzen ClearCenter portal';
+$lang['suva_app_description'] = 'Suva zerbitzuak ClearCenter atarian tunel eta enkriptatze zerbitzuak eskaintzen ditu';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'Gailuaren izena baliogabea da.';
-$lang['suva_hostkey_invalid'] = 'HosKey baliogabea da.';
+$lang['suva_device_name_invalid'] = 'Device name is not valid.';
+$lang['suva_hostkey_invalid'] = 'Hostkey baliogabea da.';
