e7173b8d1a6d7026705bdcf3825da1730c808777
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:49:52 +0000

    Add new files and directories, commit changes, push changes

--- language/ur/suva_lang.php
+++ language/ur/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'سووا ClearCenter کے پورٹل پر سرنگ اور خفیہ کاری خدمات فراہم کرتا ہے';
+$lang['suva_app_description'] = 'سووا صافیٹر پورٹل میں سرنگ اور خفیہ کاری خدمات فراہم کرتا ہے';
 $lang['suva_app_name'] = 'سووا';
-$lang['suva_device_name_invalid'] = 'ڈیوائس کا نام غلط ہے.';
+$lang['suva_device_name_invalid'] = 'آلہ کا نام غلط ہے.';
 $lang['suva_hostkey_invalid'] = 'Hostkey غلط ہے.';
