5eeedd4ec4df188d5c249daf5ce53d751be9caa9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:24:11 +0000

    Add new files and directories, commit changes, push changes

--- language/km/suva_lang.php
+++ language/km/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva ផ្លូវរូងក្រោមដីផ្តល់នូវសេវាអ៊ីនគ្រីបដើម្បីវិបផតថលនិង ClearCenter';
-$lang['suva_app_name'] = 'Suva';
+$lang['suva_app_description'] = 'Suva ផ្តល់ជូននូវសេវារូងភ្នំនិងសេវាអ៊ិនគ្រីបទៅច្រក ClearCenter';
+$lang['suva_app_name'] = 'ស៊ូវ៉ា';
 $lang['suva_device_name_invalid'] = 'ឈ្មោះឧបករណ៍មិនត្រឹមត្រូវ។';
-$lang['suva_hostkey_invalid'] = 'Hostkey មិនត្រឹមត្រូវ។';
+$lang['suva_hostkey_invalid'] = 'ម៉ាស៊ីន Host មិនត្រឹមត្រូវ។';
