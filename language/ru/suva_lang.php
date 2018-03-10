41769d48e5288d9d3d88b176fd8dad73ecaff3ee
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:38:49 +0000

    Add new files and directories, commit changes, push changes

--- language/ru/suva_lang.php
+++ language/ru/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Сува предоставляет туннель и шифрования услуги портала ClearCenter';
+$lang['suva_app_description'] = 'Сува предоставляет услуги туннеля и шифрования для порта ClearCenter';
 $lang['suva_app_name'] = 'Сува';
-$lang['suva_device_name_invalid'] = 'Имя устройства является недействительным.';
-$lang['suva_hostkey_invalid'] = 'HOSTKEY является недействительным.';
+$lang['suva_device_name_invalid'] = 'Имя устройства недействительно.';
+$lang['suva_hostkey_invalid'] = 'Недопустимый хост-ключ.';
