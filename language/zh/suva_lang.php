0e9ed5945b8a444314a097b1a5165d34dcff1dac
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:09:00 +0000

    Add new files and directories, commit changes, push changes

--- language/zh/suva_lang.php
+++ language/zh/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = '苏瓦ClearCenter门户网站提供隧道和加密服务';
+$lang['suva_app_description'] = 'Suva为ClearCenter门户提供隧道和加密服务';
 $lang['suva_app_name'] = '苏瓦';
-$lang['suva_device_name_invalid'] = '设备名称是无效的。';
-$lang['suva_hostkey_invalid'] = 'HOSTKEY无效。';
+$lang['suva_device_name_invalid'] = '设备名称无效。';
+$lang['suva_hostkey_invalid'] = '主机密钥无效。';
