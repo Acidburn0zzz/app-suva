82718fd8fe7a44e4638861fcec9c43fe00020715
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:44:15 +0000

    Add new files and directories, commit changes, push changes

--- language/sw/suva_lang.php
+++ language/sw/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva hutoa handaki na encryption huduma kwa ClearCenter portal';
+$lang['suva_app_description'] = 'Suva hutoa huduma na huduma za encryption kwa portal ClearCenter';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'jina kifaa ni batili.';
+$lang['suva_device_name_invalid'] = 'Jina la kifaa ni batili.';
 $lang['suva_hostkey_invalid'] = 'Hostkey ni batili.';
