2db20265874ca85edba9fc939e1c06e9d4a8667c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:26:08 +0000

    Add new files and directories, commit changes, push changes

--- language/la/suva_lang.php
+++ language/la/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Encryption Suva praebet et curriculum operam ClearCenter Portal';
-$lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'Fabrica nomen est invalidum.';
-$lang['suva_hostkey_invalid'] = 'Hostkey est invalidum.';
+$lang['suva_app_description'] = 'Suva praebet curriculum et officia encryption ut ClearCenter portal';
+$lang['suva_app_name'] = 'suva';
+$lang['suva_device_name_invalid'] = 'Fabrica nomen invalidum est.';
+$lang['suva_hostkey_invalid'] = 'Hostkey invalidum est.';
