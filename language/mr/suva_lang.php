335300ab3ccf499d59610a7736e4d9e63416e2d1
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:31:46 +0000

    Add new files and directories, commit changes, push changes

--- language/mr/suva_lang.php
+++ language/mr/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva ClearCenter पोर्टल बोगदा आणि कूटबद्धीकरण सेवा उपलब्ध';
+$lang['suva_app_description'] = 'Suva ClearCenter पोर्टलला बोगदा आणि एन्क्रिप्शन सेवा प्रदान करते';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'साधन नाव अवैध आहे.';
-$lang['suva_hostkey_invalid'] = 'Hostkey अवैध आहे.';
+$lang['suva_device_name_invalid'] = 'डिव्हाइसचे नाव अवैध आहे';
+$lang['suva_hostkey_invalid'] = 'होस्टकी अवैध आहे';
