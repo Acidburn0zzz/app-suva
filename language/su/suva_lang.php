efd508ec23229597a3fbdcc49007119279a00825
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:43:48 +0000

    Add new files and directories, commit changes, push changes

--- language/su/suva_lang.php
+++ language/su/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva nyadiakeun torowongan jeung enkripsi jasa ka ClearCenter portal';
+$lang['suva_app_description'] = 'Suva nyadiakeun torowongan sarta enkripsi jasa mun ClearCenter portal';
 $lang['suva_app_name'] = 'Suva';
-$lang['suva_device_name_invalid'] = 'ngaran alat anu sah.';
+$lang['suva_device_name_invalid'] = 'Ngaran alat anu sah.';
 $lang['suva_hostkey_invalid'] = 'Hostkey téh sah.';
