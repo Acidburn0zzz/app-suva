924a935811854b683a51221086182c1b666b6381
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 11:12:34 +0000

    Add new files and directories, commit changes, push changes

--- language/et/suva_lang.php
+++ language/et/suva_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['suva_app_description'] = 'Suva annab tunnel ja krüpteerimine teenuseid ClearCenter portaali';
+$lang['suva_app_description'] = 'Suva pakub tunnelite ja krüpteerimisteenuseid ClearCenteri portaalile';
 $lang['suva_app_name'] = 'Suva';
 $lang['suva_device_name_invalid'] = 'Seadme nimi on kehtetu.';
-$lang['suva_hostkey_invalid'] = 'Hostkey on kehtetu.';
+$lang['suva_hostkey_invalid'] = 'Hostkiht on kehtetu.';
